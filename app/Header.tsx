350521022428b863741e3d28649069e01fc7ff06
Pav <pav0107@gmail.com>
2023-01-01 21:18:10 +0000

    add searchbox

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import NavLinks from './NavLinks';
+import SearchBox from './SearchBox';
 
 function Header() {
   return (
     <header>
-      <div className="grid grid-cols-3 p-10 items-center">
-        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+      <div className="grid items-center grid-cols-3 p-10">
+        <Bars3Icon className="w-8 h-8 cursor-pointer" />
         <Link href="/" prefetch={false}>
           <h1 className="text-4xl text-center">News</h1>
         </Link>
         <div className="flex items-center justify-end space-x-2">
           {/* DarkModeButton */}
-          <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
+          <button className="hidden px-4 py-2 text-white rounded-full md:inline bg-slate-900 lg:px-8 lg:py-4 dark:bg-slate-800">
             Subscribe Now
           </button>
         </div>
@@ -23,6 +24,7 @@ function Header() {
       <NavLinks />
 
       {/* SearchBox */}
+      <SearchBox />
     </header>
   );
 }
---
+++ app/SearchBox.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+
+function SearchBox() {
+  const [input, setInput] = useState('');
+  const router = useRouter();
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input) return;
+
+    router.push(`/search?term=${input}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex items-center justify-between max-w-6xl px-5 mx-auto"
+    >
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search Keywords ..."
+        className="flex-1 w-full text-gray-500 placeholder-gray-500 bg-transparent rounded-sm outline-none h-14 dark:text-orange-400"
+      />
+
+      <button
+        type="submit"
+        disabled={!input}
+        className="text-orange-400 disabled:text-gray-400"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchBox;
